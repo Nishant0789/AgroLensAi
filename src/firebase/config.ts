2e4e775485e309d8a23a1bdec0307ce7359ccf63
Nishant Yadav <Nishantofficial789@gmail.com>
2025-12-23 14:24:16 +0000

    so tell me, the project id is agrolens-6c9e5

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,10 +4,10 @@
 // The content of this file is automatically updated when you switch projects.
 import { FirebaseOptions } from 'firebase/app';
 export const firebaseConfig: FirebaseOptions = {
-  "projectId": "agrolens-ai-35681",
-  "appId": "1:107298342894:web:08637739567117ef102a0a",
-  "storageBucket": "agrolens-ai-35681.appspot.com",
-  "apiKey": "AIzaSyCb45a6wZfN8A5Tso7D5sXb-EGC1s-dIqY",
-  "authDomain": "agrolens-ai-35681.firebaseapp.com",
-  "messagingSenderId": "107298342894"
+  "projectId": "agrolens-6c9e5",
+  "appId": "1:318898165278:web:75e4299b923f18e9395627",
+  "storageBucket": "agrolens-6c9e5.appspot.com",
+  "apiKey": "AIzaSyBe16L1PZqS3y0g0gNf_0e2dJ7B-5dRrYk",
+  "authDomain": "agrolens-6c9e5.firebaseapp.com",
+  "messagingSenderId": "318898165278"
 };
