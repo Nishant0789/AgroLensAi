997b942aa6c173d67a4595ef36369572a4820ae0
Nishant Yadav <Nishantofficial789@gmail.com>
2025-12-23 14:27:58 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,10 +4,11 @@
 // The content of this file is automatically updated when you switch projects.
 import { FirebaseOptions } from 'firebase/app';
 export const firebaseConfig: FirebaseOptions = {
-  "projectId": "agrolens-6c9e5",
-  "appId": "1:318898165278:web:75e4299b923f18e9395627",
-  "storageBucket": "agrolens-6c9e5.appspot.com",
-  "apiKey": "AIzaSyBe16L1PZqS3y0g0gNf_0e2dJ7B-5dRrYk",
+  "apiKey": "AIzaSyBygqjH4RXc2DDCAF3wgfxJtty4q1jYAuY",
   "authDomain": "agrolens-6c9e5.firebaseapp.com",
-  "messagingSenderId": "318898165278"
+  "projectId": "agrolens-6c9e5",
+  "storageBucket": "agrolens-6c9e5.appspot.com",
+  "messagingSenderId": "350182602842",
+  "appId": "1:350182602842:web:baa19e71ab24d4dbc0c063",
+  "measurementId": "G-ETTJRPG07L"
 };
